4a1835cad21bb57af2b2f06477900271d740fdd7
WeHusky <120331972+WeHusky@users.noreply.github.com>
2024-07-24 23:52:43 +0700

    Add files via upload

---
+++ scrollreveal.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.container_hero', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px'
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+
+    ScrollReveal().reveal('#bimbim .card' , {
+      duration: 1000,
+      origin: 'bottom',
+      distance: '50px',
+      easing: 'ease-in-out',
+      reset: true
+    });
+  });
